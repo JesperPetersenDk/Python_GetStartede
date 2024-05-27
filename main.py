29efe404487c06086f96584570d1f78dc2f7a3b6
Jesper Petersen <info@ordklar.dk>
2024-05-27 16:12:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ def helloName():
     name = print("Hej", input("What is your name?"))
     return name
 
-helloName();
+def addNumber(numberInput):
+    ex = sum(numberInput)
+    print(ex)
+
+#helloName();
+
+expNumber = [22.2, 55.2, 66.2, 88.2, 103.3, 140.51]
+addNumber(expNumber)
